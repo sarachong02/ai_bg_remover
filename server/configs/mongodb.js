dd034184ea212deaa9af7a3b25e237c5505440c8
schong6 <schong6@uci.edu>
2024-10-29 14:04:13 -0700

    trying something new for vercel connection

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -1,3 +1,5 @@
+// let isConnected = false;
+
 // import mongoose from "mongoose";
 
 // const connectDB = async () => {
@@ -16,15 +18,39 @@ import mongoose from "mongoose";
 let isConnected = false;
 
 const connectDB = async () => {
-  if (isConnected) return;
+    if (isConnected) return;
 
-  try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/bg-removal`);
-    isConnected = true;
-    console.log("Database connected");
-  } catch (error) {
-    console.error("Could not connect to MongoDB", error);
-  }
+    mongoose.connection.on('connected', () => {
+        console.log("Database connected");
+        isConnected = true;
+    });
+
+    mongoose.connection.on('error', (error) => {
+        console.error("Database connection error:", error);
+    });
+
+    await mongoose.connect(process.env.MONGODB_URI, {
+        dbName: "bg-removal"
+    });
 };
 
 export default connectDB;
+
+
+// import mongoose from "mongoose";
+
+// let isConnected = false;
+
+// const connectDB = async () => {
+//   if (isConnected) return;
+
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URI}/bg-removal`);
+//     isConnected = true;
+//     console.log("Database connected");
+//   } catch (error) {
+//     console.error("Could not connect to MongoDB", error);
+//   }
+// };
+
+// export default connectDB;
